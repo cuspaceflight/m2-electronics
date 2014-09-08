43c224ffa4ac43ec9bc4632a868f8fb6c23b9461
Adam Greig <adam@adamgreig.com>
2014-09-08 18:11:43 +0100

    add times to analyse.py

--- log_analysis/analyse.py
+++ log_analysis/analyse.py
@@ -9,11 +9,14 @@ def main():
         print("Usage: {} <logfile>".format(sys.argv[0]))
         return
 
+    baro_t = []
     pressures = []
     temperatures = []
+    hg_accel_t = []
     hg_accel_x = []
     hg_accel_y = []
     hg_accel_z = []
+    lg_accel_t = []
     lg_accel_x = []
     lg_accel_y = []
     lg_accel_z = []
@@ -42,22 +45,50 @@ def main():
             data = struct.unpack("HHHH", packet[8:])
 
         if channel == 0x10:
+            lg_accel_t.append(timestamp)
             lg_accel_x.append(data[0] / 265.)
             lg_accel_y.append(data[1] / 265.)
             lg_accel_z.append(data[2] / 256.)
         if channel == 0x20:
+            hg_accel_t.append(timestamp)
             hg_accel_x.append(data[0] / 256.)
             hg_accel_y.append(data[1] / 256.)
             hg_accel_z.append(data[2] / 256.)
         if channel == 0x30:
+            baro_t.append(timestamp)
             pressures.append(data[0])
             temperatures.append(data[1])
 
-    plt.plot(lg_accel_x)
-    plt.plot(lg_accel_y)
-    plt.plot(lg_accel_z)
-    plt.show()
+    lg_accel_t = np.array(lg_accel_t)
+    hg_accel_t = np.array(hg_accel_t)
+    baro_t = np.array(baro_t)
 
+    lg_t_d = np.diff(lg_accel_t.T)
+    hg_t_d = np.diff(hg_accel_t.T)
+    baro_t_d = np.diff(baro_t.T)
+
+    lg_t_d = lg_t_d[lg_t_d > 0]
+    hg_t_d = hg_t_d[hg_t_d > 0]
+    baro_t_d = baro_t_d[baro_t_d > 0]
+
+    print("Mean time between LG samples:", lg_t_d.mean())
+    print("std:", np.std(lg_t_d))
+    print("")
+
+    print("Mean time between HG samples:", hg_t_d.mean())
+    print("std:", np.std(hg_t_d))
+    print("")
+
+    print("Mean time between baro samples:", baro_t_d.mean())
+    print("std:", np.std(baro_t_d))
+    print("")
+
+    t = lg_accel_t / 168E6
+
+    plt.plot(t[10000:10100], lg_accel_x[10000:10100], '-x')
+    plt.plot(t[10000:10100], lg_accel_y[10000:10100], '-x')
+    plt.plot(t[10000:10100], lg_accel_z[10000:10100], '-x')
+    plt.show()
 
 if __name__ == "__main__":
     main()
