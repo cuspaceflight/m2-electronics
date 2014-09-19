82e742d3f69cc6758af95d447660889e63b4f5c4
Fergus Noble <fergusnoble@gmail.com>
2014-09-19 14:05:32 -0700

    RockBLOCK simple dispatcher implemented, not tested!

--- m2r/firmware/dispatch.c
+++ m2r/firmware/dispatch.c
@@ -6,8 +6,15 @@
 
 #include "dispatch.h"
 
+#include "rockblock.h"
+
 void dispatch_pvt(const ublox_pvt_t pvt)
 {
+  static systime_t last_sbd = 0;
+  if (chTimeElapsedSince(last_sbd) > S2ST(30)) {
+    /* Only send an Iridium SBD message every 30s */
+    send_sbd_posn(&pvt);
+  }
 }
 
 msg_t dispatch_thread(void* arg)
--- m2r/firmware/rockblock.c
+++ m2r/firmware/rockblock.c
@@ -5,3 +5,30 @@
  */
 
 #include "rockblock.h"
+#include "chprintf.h"
+
+void rockblock_init()
+{
+  const SerialConfig iridium_port_config = {
+    .speed = 19200
+  };
+
+  sdInit();
+  sdStart(&ROCKBLOCK_PORT, &iridium_port_config);
+}
+
+void send_sbd_posn(const ublox_pvt_t *pvt)
+{
+  chprintf((BaseSequentialStream *)&ROCKBLOCK_PORT, "AT+SBDD0\r\n");
+  chThdSleepMilliseconds(500);
+  chprintf((BaseSequentialStream *)&ROCKBLOCK_PORT,
+    "AT+SBDWT=M2R: %02d:%02d:%02d 0x%02X %.3f,%.3f,%.3f %.1f\r\n",
+    pvt->hour, pvt->minute, pvt->second,
+    pvt->fix_type,
+    pvt->lat*1e-7, pvt->lon*1e-7, pvt->height*1e-3,
+    -pvt->velD*1e-3
+  );
+  chThdSleepMilliseconds(500);
+  chprintf((BaseSequentialStream *)&ROCKBLOCK_PORT, "AT+SBDI\r\n");
+}
+
