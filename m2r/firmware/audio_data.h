44edcb0b0668bdc52464a15f24d646c99bc2d4ae
Fergus Noble <fergusnoble@gmail.com>
2014-09-19 23:35:38 -0700

    Initial import of audio data

---
+++ m2r/firmware/audio_data.h
@@ -0,0 +1,24 @@
+/*
+ * Audio sample data
+ * M2R
+ * 2014 Fergus Noble, Cambridge University Spaceflight
+ */
+
+#ifndef AUDIO_DATA_H
+#define AUDIO_DATA_H
+
+extern const uint8_t one[2968];
+extern const uint8_t two[2968];
+extern const uint8_t three[3710];
+extern const uint8_t four[3710];
+extern const uint8_t five[4452];
+extern const uint8_t six[4452];
+extern const uint8_t seven[5194];
+extern const uint8_t eight[2968];
+extern const uint8_t nine[4452];
+extern const uint8_t kft[7420];
+extern const uint8_t drogue[8162];
+extern const uint8_t main_chute[10388];
+
+
+#endif /* AUDIO_DATA_H */
