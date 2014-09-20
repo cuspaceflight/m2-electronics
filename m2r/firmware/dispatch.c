8f842d86468de0cc3c0567c19ec55d2a586c359b
Fergus Noble <fergusnoble@gmail.com>
2014-09-20 00:19:37 -0700

    Add function to say an altitude using audio samples.

--- m2r/firmware/dispatch.c
+++ m2r/firmware/dispatch.c
@@ -4,10 +4,41 @@
  * 2014 Adam Greig, Cambridge University Spaceflight
  */
 
+#include <math.h>
+#include "ch.h"
+#include "chprintf.h"
+
 #include "dispatch.h"
+#include "audio_data.h"
 
 void dispatch_pvt(const ublox_pvt_t pvt)
 {
+}
+
+uint8_t * const number[10] = {
+  zero, one, two, three, four, five, six, seven, eight, nine
+};
+
+/* alt is in meters AMSL */
+void say_altitude(float alt, uint8_t **samples)
+{
+    uint32_t kft_agl = lroundf((3.2808399f * alt - 4000.0f) / 1000.0f);
+
+    if (kft_agl == 0) {
+        /* below 1000 ft, don't report altitude */
+        return;
+    }
+
+    char buff[4] = {0};
+    chsnprintf(buff, 4, "%-3d", kft_agl);
+
+    uint8_t i = 0;
+    while (buff[i] && buff[i] != ' ') {
+        samples[i] = number[buff[i] - '0'];
+        i++;
+    }
+    samples[i++] = kft;
+    samples[i] = 0;
 }
 
 msg_t dispatch_thread(void* arg)
