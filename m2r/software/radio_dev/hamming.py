93113dfeecd0e90d679286749d476259476aa92b
Adam Greig <adam@adamgreig.com>
2015-08-13 23:40:13 +0100

    update ecc; works but isn't very good

--- m2r/software/radio_dev/hamming.py
+++ m2r/software/radio_dev/hamming.py
@@ -2,15 +2,20 @@ from __future__ import print_function
 import numpy as np
 
 G = np.array((
-    (1, 0, 0, 0, 0, 1, 1),
-    (0, 1, 0, 0, 1, 0, 1),
-    (0, 0, 1, 0, 1, 1, 0),
-    (0, 0, 0, 1, 1, 1, 1))).T
+    (1, 0, 0, 0),
+    (0, 1, 0, 0),
+    (0, 0, 1, 0),
+    (0, 0, 0, 1),
+    (1, 1, 1, 0),
+    (0, 1, 1, 1),
+    (1, 0, 1, 1)))
 
 H = np.array((
-    (0, 0, 0, 1, 1, 1, 1),
-    (0, 1, 1, 0, 0, 1, 1),
-    (1, 0, 1, 0, 1, 0, 1)))
+    (1, 1, 1, 0, 1, 0, 0),
+    (0, 1, 1, 1, 0, 1, 0),
+    (1, 0, 1, 1, 0, 0, 1)))
+
+bitflips = (None, 7, 6, 4, 5, 1, 2, 3)
 
 codewords = []
 for nibble in range(16):
@@ -28,13 +33,18 @@ print()
 datanibbles = []
 for byte in range(128):
     byte_b = np.binary_repr(byte, width=7)
-    byte_a = list(int(d) for d in byte_b)
+    byte_a = np.array(list(int(d) for d in byte_b))
     syndrome_b = np.dot(H, byte_a) % 2
     syndrome = np.packbits(syndrome_b)[0] >> 5
     if syndrome:
-        byte ^= (1 << (syndrome - 1))
+        byte ^= (1 << (7 - bitflips[syndrome]))
     datanibble = byte >> 3
     datanibbles.append(datanibble)
+    note = "*"*30 if syndrome == 0 else ""
+    if None:
+        print("{}: y={:02} z={}={} x={} {}"
+              .format(byte_b, datanibble, syndrome_b, syndrome, datanibble,
+                      note))
 print("static const uint8_t hamming_7_4_datanibbles[128] = {")
-print(", ".join(list("0x{:02x}".format(nibble) for nibble in datanibbles)))
+print(", ".join(list("0x{:01x}".format(nibble) for nibble in datanibbles)))
 print("};")
--- m2r/software/radio_dev/main.c
+++ m2r/software/radio_dev/main.c
@@ -2,35 +2,87 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "hamming_ecc.h"
 
-int main(int argc, char* argv[]) {
-    size_t i;
-    uint8_t data_tx[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    uint8_t data_rx[16];
+bool run_trial(double ber, unsigned int n_errors, size_t *nerrs) {
+    size_t i, j;
+    uint8_t data_tx[16], data_rx[16];
     uint8_t coded_tx[32], coded_rx[32];
     uint8_t interleaved_tx[28], interleaved_rx[28];
 
-    (void)argc;
-    (void)argv;
-
-    printf("Transmitting:\n");
     for(i=0; i<sizeof(data_tx); i++) {
-        printf("0x%02X ", data_tx[i]);
+        data_tx[i] = rand() & 0xFF;
     }
-    printf("\n");
 
     data_to_coded(data_tx, coded_tx);
     coded_to_interleaved(coded_tx, interleaved_tx);
-    memcpy(interleaved_rx, interleaved_rx, sizeof(interleaved_rx));
+    memcpy(interleaved_rx, interleaved_tx, sizeof(interleaved_rx));
+
+    for(i=0; i<sizeof(interleaved_rx); i++) {
+        for(j=0; j<8; j++) {
+            double x = (double)rand() / (double)RAND_MAX;
+            if(x < ber) {
+                (*nerrs)++;
+                interleaved_rx[i] ^= (1<<j);
+            }
+        }
+    }
+
+    for(i=0; i<n_errors; i++) {
+        int error_pos = rand() % (sizeof(interleaved_rx) * 8);
+        (*nerrs)++;
+        interleaved_rx[error_pos / 8] ^= (1<<(error_pos % 8));
+    }
+
     interleaved_to_coded(interleaved_rx, coded_rx);
     coded_to_data(coded_rx, data_rx);
 
-    printf("Received:\n");
     for(i=0; i<sizeof(data_rx); i++) {
-        printf("0x%02X ", data_rx[i]);
+        if(data_rx[i] != data_tx[i]) {
+            return false;
+        }
     }
-    printf("\n");
+    
+    return true;
+}
+
+double run_trials(double ber, unsigned int n_errors) {
+    int i;
+    int successes = 0;
+    size_t n_errs = 0;
+    const int num_trials = 500000;
+    for(i=0; i<num_trials; i++) {
+        if(run_trial(ber, n_errors, &n_errs)) {
+            successes++;
+        }
+    }
+    printf("avg num errs: %.04f\n", (double)n_errs / num_trials);
+    /*printf("%d successes out of %d trials\n", successes, num_trials);*/
+    return 1.0 - ((double)successes / (double)num_trials);
+}
+
+int main(int argc, char* argv[]) {
+    (void)argc;
+    (void)argv;
+    int i;
+
+    srand(0);
+
+    printf("\n#err\tBER\tPerr\n");
+    for(i=0; i<21; i++) {
+        double perr = run_trials(-1.0f, i);
+        double ber = (double)i / 224.0f;
+        printf("%d\t%.04f\t%.04f\n", i, ber, perr);
+    }
+
+
+    printf("\nBER\t#err\tPerr\n");
+    for(i=0; i<210; i++) {
+        double ber = (double)i / 1000.0f;
+        double perr = run_trials(ber, 0);
+        printf("%.03f\t%.04f\n", ber, perr);
+    }
 
     return 0;
 }
