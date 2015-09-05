06ffc7a5ba266c84582b91b5f90d57d327b103d2
Adam Greig <adam@adamgreig.com>
2015-09-05 17:48:32 +0100

    update makefile and compare for new c libldpcd

--- m2r/software/radio_dev/compare.py
+++ m2r/software/radio_dev/compare.py
@@ -1,14 +1,15 @@
 import time
 import ctypes
 import numpy as np
-clib = ctypes.cdll.LoadLibrary("./libcldpcd.so")
-rlib = ctypes.cdll.LoadLibrary("./libldpcd.so")
+newclib = ctypes.cdll.LoadLibrary("./libnewcldpcd.so")
+#rlib = ctypes.cdll.LoadLibrary("./libldpcd.so")
 dbl_ptype = np.ctypeslib.ndpointer(dtype=np.double, ndim=1)
 u8_ptype = np.ctypeslib.ndpointer(dtype=np.uint8, ndim=1)
-cdecode = clib.ldpc_decode
-cdecode.argtypes = [dbl_ptype, u8_ptype]
-rdecode = rlib.ldpc_decode
-rdecode.argtypes = [dbl_ptype, u8_ptype]
+newcdecode = newclib.ldpc_decode
+newcdecode.argtypes = [dbl_ptype, u8_ptype]
+#rdecode = rlib.ldpc_decode
+#rdecode.argtypes = [dbl_ptype, u8_ptype]
+newclib.ldpc_init()
 
 Wh = np.unpackbits(np.array((
     (0x73, 0xF5, 0xE8, 0x39,
@@ -38,31 +39,69 @@ for row in range(128):
 Wp = np.packbits(W.T, axis=1)
 G = np.hstack((np.eye(128), W))
 
-snr = 10
+repeats = 100
+snr_db = 3.0
+snr = 10.0**(snr_db/10.0)
 sigma = np.sqrt(1.0 / snr)
+#np.random.seed(1)
 
-np.random.seed(0)
-times = []
-for repeat in range(500):
-    msg = np.unpackbits(np.random.randint(0, 256, 16).astype(np.uint8))
-    cw = np.dot(msg, G) % 2
-    llr = -1 * ((2 * cw.astype(np.double)) - 1) + np.random.randn(256) * sigma
-    result = np.empty(32, np.uint8)
-    t0 = time.time()
-    cdecode(llr, result)
-    t1 = time.time()
-    times.append(t1 - t0)
-print("Avg time, snr {}, 500 msgs, C   :".format(snr), np.mean(times))
+c_times = []
+r_times = []
+c_errors = []
+r_errors = []
+for repeat in range(repeats):
 
-np.random.seed(0)
-times = []
-for repeat in range(500):
-    msg = np.unpackbits(np.random.randint(0, 256, 16).astype(np.uint8))
+    np.random.seed(repeat)
+
+    packed = np.random.randint(0, 256, 16).astype(np.uint8)
+    msg = np.unpackbits(packed)
     cw = np.dot(msg, G) % 2
-    llr = -1 * ((2 * cw.astype(np.double)) - 1) + np.random.randn(256) * sigma
-    result = np.empty(32, np.uint8)
+    noise = np.random.randn(256) * sigma
+    x = 2 * cw.astype(np.double) - 1
+    y = x + noise
+    p_y_xp = np.exp(-(y-1)**2 / (2*sigma**2))
+    p_y_xm = np.exp(-(y+1)**2 / (2*sigma**2))
+    p_x1_y = p_y_xp / (p_y_xp + p_y_xm)
+    p_x0_y = 1 - p_x1_y
+    r_llr = np.log(p_x0_y / p_x1_y)
+    c_llr = r_llr[:]
+
+    #r_result = np.zeros(16, np.uint8)
+    #t0 = time.time()
+    #rdecode(r_llr, r_result)
+    #t1 = time.time()
+    #r_times.append(t1 - t0)
+    #if not np.all(r_result == packed):
+        #r_errors.append(repeat)
+
+    c_result = np.zeros(16, np.uint8)
     t0 = time.time()
-    rdecode(llr, result)
+    newcdecode(c_llr, c_result)
     t1 = time.time()
-    times.append(t1 - t0)
-print("Avg time, snr {}, 500 msgs, Rust:".format(snr), np.mean(times))
+    c_times.append(t1 - t0)
+    if not np.all(c_result == packed):
+        c_errors.append(repeat)
+
+    if np.all(c_result == 0xFF) or np.all(c_result == 0x00):
+        print("Repeat", repeat)
+        print("Packed:  ", packed)
+        print("c_result:", c_result)
+
+    #if repeat in set(c_errors) - set(r_errors):
+
+        #print("Repeat", repeat)
+        #print("Packed:  ", packed)
+        #print("r_result:", r_result)
+        #print("c_result:", c_result)
+        #print("LLRs:")
+        #print("{", ", ".join("{:.60f}".format(f) for f in c_llr), "}")
+
+#print("Rust: SNR {:.1f}dB, PER {:.4e}, {} repeats, Avg Time {:.6f}s"
+      #.format(10*np.log10(snr), len(r_errors)/repeats, repeats,
+              #np.mean(r_times)))
+
+print("NewC: SNR {:.1f}dB, PER {:.4e}, {} repeats, Avg Time {:.6f}s"
+      .format(10*np.log10(snr), len(c_errors)/repeats, repeats,
+              np.mean(c_times)))
+
+print("Repeats that differed:", set(c_errors) - set(r_errors))
