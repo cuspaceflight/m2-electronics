2b3e039140bff2f5f2b598fce44b9ddc672e426e
Adam Greig <adam@adamgreig.com>
2015-09-19 11:21:28 +0100

    update shell commands a bit

--- m2fc/firmware/m2fc_shell.c
+++ m2fc/firmware/m2fc_shell.c
@@ -6,6 +6,7 @@
  */
 #include "m2fc_shell.h"
 #include <hal.h>
+#include <string.h>
 #include "chprintf.h"
 #include "hmc5883l.h"
 #include "l3g4200d.h" 
@@ -39,13 +40,16 @@ static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[]) {
     return;
   }
 
-  chprintf(chp, "name          addr    stack prio refs     state   time\r\n");
-  chprintf(chp, "------------------------------------------------------\r\n");
+  chprintf(chp,
+    "name        |addr    |stack   |free|prio|refs|state    |time\r\n");
+  chprintf(chp,
+    "------------|--------|--------|----|----|----|---------|--------\r\n");
   tp = chRegFirstThread();
   do {
-    chprintf(chp, "%10s %.8lx %.8lx %4lu %4lu %9s %lu\r\n",
+    chprintf(chp, "%12s|%.8lx|%.8lx|%4lu|%4lu|%4lu|%9s|%lu\r\n",
             chRegGetThreadName(tp),
             (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
+            (uint32_t)tp->p_ctx.r13 - (uint32_t)tp->p_stklimit,
             (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
             states[tp->p_state], (uint32_t)tp->p_time);
     if(tp->p_prio != 1) {
@@ -73,15 +77,12 @@ static void cmd_gyro(BaseSequentialStream *chp, int argc, char *argv[]) {
     (void)argc;	
     (void)argv;
 
-    for(;;) {
-        /*chprintf(chp," x: %09d, y: %09d, z: %09d \n", global_gyro[0], global_gyro[1], global_gyro[2]);*/
-        chprintf(chp,"%09d %09d %09d \n", global_gyro[0], global_gyro[1], global_gyro[2]);
-        chThdSleepMilliseconds(1);
-    }
+    chprintf(chp, "Gyro readings:\n  x         y         z\n");
+    chprintf(chp,"%09d %09d %09d\n", global_gyro[0], global_gyro[1], global_gyro[2]);
     return;
 }
 
-static void cmd_barotest(BaseSequentialStream *chp, int argc, char *argv[]) {
+static void cmd_baro(BaseSequentialStream *chp, int argc, char *argv[]) {
     (void)argv;
     (void)argc;
     
@@ -89,29 +90,23 @@ static void cmd_barotest(BaseSequentialStream *chp, int argc, char *argv[]) {
     chprintf(chp, "Current Temperature: %d\r\n", temperature);
 }
 
-static void cmd_magnotest(BaseSequentialStream *chp, int argc, char *argv[]) {
+static void cmd_magno(BaseSequentialStream *chp, int argc, char *argv[]) {
     (void)argv;
     (void)argc;
-    for(;;) {
-        /* chprintf(chp, "Magno X: 0x%x %d\r\n", global_magno[0]);
-        chprintf(chp, "Magno Y: 0x%x %d\r\n", global_magno[1]);
-        chprintf(chp, "Magno Z: 0x%x %d\r\n", global_magno[2]); */
-        chprintf(chp,"%09d %09d %09d \n", global_magno[0], global_magno[1], global_magno[2]);
-        chThdSleepMilliseconds(100);
-    }
+    chprintf(chp, "Magno readings:\n  x         y         z\n");
+    chprintf(chp,"%09d %09d %09d \n", global_magno[0], global_magno[1], global_magno[2]);
 }
 
 static void cmd_accel(BaseSequentialStream *chp, int argc, char *argv[]) {
     (void)argc;	
     (void)argv;
-    /*Should probably convert using adxl3x5_accels_to_axis */
     
+    chprintf(chp, "Accel readings:\n x     y     z\n");
     chprintf(chp," x: %d, y: %d, z: %d \n", accels[0], accels[1], accels[2]);
     return;
 }
 
 static void cmd_pyro(BaseSequentialStream *chp, int argc, char *argv[]) {
-    (void)argv;
     bool p1, p2, p3;
     
     /* Continuity Check */
@@ -122,37 +117,14 @@ static void cmd_pyro(BaseSequentialStream *chp, int argc, char *argv[]) {
     
     chprintf(chp, "Results of Continuity Check: 1:%u, 2:%u, 3:%u\n",
              p1, p2, p3);
-                                                    
-    if (argc > 0) 
-    {
-        /*if (argv[0][0] == "yes")*/
-        /*{*/
-            /* if (argv[0][1] == '1') 
-            {
-               pyro_fire(1, 1000);
-            }
-            
-            else if (argv[0][1] == '2') 
-            {
-                pyro_fire(2, 1000);
-            }
-            
-            else if (argv[0][1] == '3') 
-            {
-                pyro_fire(3, 1000);
-            }  
-            
-            else
-            {
-                chprintf(chp, "Please select which pyro you want to fire by writing pyro yes [number]");
-            }
-            */
-        /*}*/
-        
-        /*else*/
-        /*{*/
-        /*chprintf(chp, "Please confirm you want to test Pyros by writing yes as first argument");*/
-        /*}*/
+
+    if(argc == 1 && strcmp(argv[0], "fire") == 0) {
+        int i;
+        for(i=5; i>0; i--) {
+            chprintf(chp, "Firing pyros in %d...\n", i);
+            chThdSleepMilliseconds(1000);
+        }
+        pyro_fire(true, true, true);
     }
 }
 
@@ -162,11 +134,11 @@ void m2fc_shell_run()
         {"mem", cmd_mem},
         {"threads", cmd_threads},
         {"rt", cmd_rt},
-        {"magno", cmd_magnotest},
+        {"magno", cmd_magno},
         {"gyro", cmd_gyro},
-        {"baro", cmd_barotest},
+        {"baro", cmd_baro},
         {"accel", cmd_accel},
-        {"baro", cmd_pyro},
+        {"pyro", cmd_pyro},
         {NULL, NULL}
     };
     static const ShellConfig shell_cfg = {
